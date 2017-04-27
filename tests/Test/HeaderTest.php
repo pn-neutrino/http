6bbaca29605b788e63384533c325eed8bccf2a1e
Ark4ne <gui.allegret@gmail.com>
2017-04-27 19:23:34 +0200

    full Header Test

--- tests/Test/HeaderTest.php
+++ tests/Test/HeaderTest.php
@@ -6,32 +6,96 @@ use Neutrino\Http\Header;
 
 class HeaderTest extends \PHPUnit\Framework\TestCase
 {
+    public function testGetSetHasRemoveCount()
+    {
+        $header = new Header();
+
+        $this->assertEquals($header, $header->set('test', 'test'));
+
+        $header->set('test', 'value');
+        $header->set('test_1', 'value_1');
+
+        $this->assertEquals(['test' => 'value', 'test_1' => 'value_1'], $header->getHeaders());
+
+        $this->assertTrue($header->has('test'));
+        $this->assertTrue($header->has('test_1'));
+        $this->assertFalse($header->has('test_2'));
+
+        $this->assertEquals('value', $header->get('test'));
+        $this->assertEquals('value_1', $header->get('test_1'));
+        $this->assertEquals(null, $header->get('test_2'));
+        $this->assertEquals('value', $header->get('test', 'default'));
+        $this->assertEquals('default', $header->get('test_2', 'default'));
+
+        $this->assertEquals(2, $header->count());
+        $this->assertCount(2, $header);
+
+        $this->assertEquals($header, $header->remove('test'));
+
+        $this->assertEquals(null, $header->get('test'));
+
+        $this->assertEquals(1, $header->count());
+        $this->assertCount(1, $header);
+
+        $this->assertEquals($header, $header->setHeaders([
+            'test' => 'value',
+            'test_2' => 'value_2'
+        ], true));
+
+        $this->assertEquals('value_2', $header->get('test_2', 'default'));
+
+        $this->assertEquals(3, $header->count());
+        $this->assertCount(3, $header);
+
+        $this->assertEquals($header, $header->setHeaders([
+            'test' => 'value',
+            'test_2' => 'value_2'
+        ]));
+
+        $this->assertEquals(2, $header->count());
+        $this->assertCount(2, $header);
+    }
+
+    public function testBuild()
+    {
+        $header = new Header();
+
+        $header->set('test', 'test');
+        $header->set('test', 'value');
+        $header->set('test_1', 'value_1');
+
+        $this->assertEquals([
+            'test: value',
+            'test_1: value_1',
+        ], $header->build());
+    }
+
     public function dataParse()
     {
         return [
-            [null, null, [], ""],
-            [null, null, ['Date' => 'Thu, 27 Apr 2017 13:42:19 GMT'], PHP_EOL . "Date: Thu, 27 Apr 2017 13:42:19 GMT" . PHP_EOL],
-            [200, 'OK', [], "HTTP/1.1 200 OK"],
-            [200, 'Success', [], "HTTP/1.1 200 Success"],
-            [302, 'Redirect', [], "HTTP/1.1 302 Redirect"],
-            [418, 'I\'m a teapot', [], "HTTP/1.1 418 I'm a teapot"],
-            [526, 'Whoops', [], "HTTP/1.1 526 Whoops"],
-            [null, null, [
-                'Date' => 'Thu, 27 Apr 2017 13:42:19 GMT',
-                'X-Powered-By' => 'PHP/7.0.10',
+            [null, null, null, [], ""],
+            [null, null, null, ['Date' => 'Thu, 27 Apr 2017 13:42:19 GMT'], PHP_EOL . "Date: Thu, 27 Apr 2017 13:42:19 GMT" . PHP_EOL],
+            ['1.1', 200, 'OK', [], "HTTP/1.1 200 OK"],
+            ['1.1', 200, 'Success', [], "HTTP/1.1 200 Success"],
+            ['1.1', 302, 'Redirect', [], "HTTP/1.1 302 Redirect"],
+            ['1.1', 418, 'I\'m a teapot', [], "HTTP/1.1 418 I'm a teapot"],
+            ['1.1', 526, 'Whoops', [], "HTTP/1.1 526 Whoops"],
+            [null, null, null, [
+                'Date'           => 'Thu, 27 Apr 2017 13:42:19 GMT',
+                'X-Powered-By'   => 'PHP/7.0.10',
                 'Content-Length' => '5524',
-                'Server' => 'Apache/2.4.23 (Win64) PHP/7.0.10',
+                'Server'         => 'Apache/2.4.23 (Win64) PHP/7.0.10',
             ], [
-                "Date: Thu, 27 Apr 2017 13:42:19 GMT",
-                "X-Powered-By: PHP/7.0.10\r\n Content-Length: 5524",
-                "Server: Apache/2.4.23 (Win64) PHP/7.0.10",
-            ]],
-            [200, 'OK', [
-                'Date' => 'Thu, 27 Apr 2017 13:42:19 GMT',
-                'Server' => 'Apache/2.4.23 (Win64) PHP/7.0.10',
-                'X-Powered-By' => 'PHP/7.0.10',
+                 "Date: Thu, 27 Apr 2017 13:42:19 GMT",
+                 "X-Powered-By: PHP/7.0.10\r\n Content-Length: 5524",
+                 "Server: Apache/2.4.23 (Win64) PHP/7.0.10",
+             ]],
+            ['1.1', 200, 'OK', [
+                'Date'           => 'Thu, 27 Apr 2017 13:42:19 GMT',
+                'Server'         => 'Apache/2.4.23 (Win64) PHP/7.0.10',
+                'X-Powered-By'   => 'PHP/7.0.10',
                 'Content-Length' => '5524',
-                'Content-Type' => 'text/html; charset=UTF-8',
+                'Content-Type'   => 'text/html; charset=UTF-8',
             ], "HTTP/1.1 200 OK
 Date: Thu, 27 Apr 2017 13:42:19 GMT
 Server: Apache/2.4.23 (Win64) PHP/7.0.10
@@ -50,7 +114,7 @@ Content-Type: text/html; charset=UTF-8
      * @param $expectedHeaders
      * @param $raw
      */
-    public function testParse($expectedCode, $expectedStatus, $expectedHeaders, $raw)
+    public function testParse($expectedVersion, $expectedCode, $expectedStatus, $expectedHeaders, $raw)
     {
         $header = new Header();
 
@@ -62,6 +126,7 @@ Content-Type: text/html; charset=UTF-8
             $header->parse($raw);
         }
 
+        $this->assertEquals($expectedVersion, $header->version);
         $this->assertEquals($expectedCode, $header->code);
         $this->assertEquals($expectedStatus, $header->status);
         $this->assertEquals($expectedHeaders, $header->getHeaders());
